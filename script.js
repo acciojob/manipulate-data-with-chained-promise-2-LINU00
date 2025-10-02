d432c486aa1e1baf6f9175af9e69e152db405466
Aditya Singh <103933450+LINU00@users.noreply.github.com>
2025-10-03 04:01:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function getNumbers() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  });
+}
+
+getNumbers()
+  .then((numbers) => {
+    return new Promise((resolve) => {
+      const evenNumbers = numbers.filter((num) => num % 2 === 0);
+      setTimeout(() => {
+        document.getElementById("output").textContent = evenNumbers;
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+  .then((evenNumbers) => {
+    return new Promise((resolve) => {
+      const multipliedNumbers = evenNumbers.map((num) => num * 2);
+      setTimeout(() => {
+        document.getElementById("output").textContent = multipliedNumbers;
+        resolve(multipliedNumbers);
+      }, 2000);
+    });
+  });
